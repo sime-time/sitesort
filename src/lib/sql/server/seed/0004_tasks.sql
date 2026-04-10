6505a2921c3364016d0ec59596aa8fde3df6d7c9
Dunn, Simeon <simedunn01@gmail.com>
2026-04-10 19:18:36 -0400

    tasks seed

---
+++ src/lib/sql/server/seed/0004_tasks.sql
@@ -0,0 +1,7 @@
+INSERT INTO tasks (job_id, description, completed)
+VALUES
+  ('5c3bd5fe-f421-4b8e-9f59-23295b831da2', 'Test all receptacles', true),
+  ('5c3bd5fe-f421-4b8e-9f59-23295b831da2', 'Test all GFCI''s and protected receptacles', false),
+  ('5c3bd5fe-f421-4b8e-9f59-23295b831da2', 'Test all switches', false),
+  ('5c3bd5fe-f421-4b8e-9f59-23295b831da2', 'Test smoke detectors', false),
+  ('5c3bd5fe-f421-4b8e-9f59-23295b831da2', 'Verify all circuits are turned on', false);
