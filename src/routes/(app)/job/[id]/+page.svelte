0a9ecb0b10127e0c63b4cdf5ee8eb36c6360b915
Dunn, Simeon <simedunn01@gmail.com>
2026-04-14 16:48:34 -0400

    dialog modal add item

--- src/routes/(app)/job/[id]/+page.svelte
+++ src/routes/(app)/job/[id]/+page.svelte
@@ -17,6 +17,12 @@
   let loading = $state(true);
   let activeTab = $state<"materials" | "checklist">("materials");
 
+  let addModal: HTMLDialogElement | undefined;
+
+  function openAddModal() {
+    addModal?.showModal();
+  }
+
   const materialsByCategory = $derived.by(() => {
     const groups = new Map<string, JobMaterial[]>();
 
@@ -58,7 +64,7 @@
 </script>
 
 <div class="min-h-0 p-3 flex flex-col flex-1 ">
-  <!-- Tabs Header -->
+  <!-- Tab Selector -->
   <div
     role="tablist"
     class="tabs tabs-box tabs-sm w-full shrink-0 font-medium text-base"
@@ -92,7 +98,7 @@
     </button>
   </div>
 
-  <!-- Tab Content -->
+  <!-- Tab Content List -->
   <div class="tab-content flex flex-col min-h-0 bg-base-100">
     <section
       class="flex-1 min-h-0 overflow-y-auto overscroll-contain pt-3 pb-20 no-scrollbar"
@@ -111,15 +117,52 @@
         {/each}
       {/if}
 
+      <!-- Open Modal Trigger -->
       <button
         type="button"
         class="mt-6 w-full uppercase font-heading tracking-widest btn btn-xl btn-neutral btn-soft border-2 border-neutral border-dashed"
+        onclick={openAddModal}
       >
         <Icon icon="material-symbols:add-circle-outline" />
         <span class="text-base">
           {activeTab === "materials" ? "New Material" : "New Task"}
         </span>
       </button>
+
+      <!-- Modal -->
+      <dialog bind:this={addModal} class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+          <!-- Close Modal Button -->
+          <form method="dialog">
+            <button
+              type="submit"
+              class="btn btn-sm btn-circle btn-neutral btn-soft absolute right-2 top-2"
+            >
+              <Icon icon="material-symbols:close" class="size-6" />
+            </button>
+          </form>
+
+          <!-- Modal Form -->
+          <h3 class="text-lg font-bold">
+            {activeTab === "materials" ? "New Material" : "New Task"}
+          </h3>
+          <p class="py-4">Press ESC key or click the button below to close</p>
+
+          <!-- Add To Job Button -->
+          <div class="modal-action">
+            <button
+              type="button"
+              class="mt-6 w-full uppercase font-heading tracking-widest btn btn-lg btn-primary"
+            >
+              <Icon
+                icon="material-symbols:add-circle-outline"
+                class="font-bold"
+              />
+              <span class="text-base"> Add to Job </span>
+            </button>
+          </div>
+        </div>
+      </dialog>
     </section>
   </div>
 </div>
