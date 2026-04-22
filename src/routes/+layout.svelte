ef5d670d1164cc1499312fa6e51f677aa266e8c3
Dunn, Simeon <simedunn01@gmail.com>
2026-04-22 18:05:17 -0400

    remove install-pwa component for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,6 @@
   import "@fontsource-variable/space-grotesk/wght.css";
   import "@fontsource-variable/work-sans/wght.css";
   import { onMount } from "svelte";
-  import InstallPWA from "$lib/components/install/InstallPWA.svelte";
   import { Toaster } from "$lib/components/sonner";
 
   import "../app.css";
@@ -77,6 +76,5 @@
 
 <Toaster />
 <main class="w-full min-h-dvh flex justify-center">
-  <InstallPWA />
   <div class="w-full max-w-120 min-h-dvh">{@render children()}</div>
 </main>
